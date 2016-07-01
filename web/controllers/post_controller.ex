f58fdc25d0b011bde4ef8c967987e34e64dca367
Timothy Wang <timzwang@gmail.com>
2016-07-01 11:14:11 -0700

    Nest Post under Event
    * index, show, edit, update, and delete to assoc to Event if given

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -1,10 +1,25 @@
 defmodule Churchspace.PostController do
   use Churchspace.Web, :controller
 
+  alias Churchspace.Event
   alias Churchspace.Post
 
+  def action(conn, _) do
+    conn =
+      case conn.params do
+        %{"event_id" => id} ->
+          assign(conn, :event, Repo.get!(Event, id))
+        _ ->
+          conn
+      end
+    apply(__MODULE__, action_name(conn), [conn, conn.params])
+  end
+
+  defp event_posts(nil), do: Post
+  defp event_posts(%Event{} = event), do: assoc(event, :posts)
+
   def index(conn, _params) do
-    posts = Repo.all(Post)
+    posts = Repo.all(event_posts(conn.assigns[:event]))
     render(conn, "index.html", posts: posts)
   end
 
@@ -27,18 +42,18 @@ defmodule Churchspace.PostController do
   end
 
   def show(conn, %{"id" => id}) do
-    post = Repo.get!(Post, id)
+    post = Repo.get!(event_posts(conn.assigns[:event]), id)
     render(conn, "show.html", post: post)
   end
 
   def edit(conn, %{"id" => id}) do
-    post = Repo.get!(Post, id)
+    post = Repo.get!(event_posts(conn.assigns[:event]), id)
     changeset = Post.changeset(post)
     render(conn, "edit.html", post: post, changeset: changeset)
   end
 
   def update(conn, %{"id" => id, "post" => post_params}) do
-    post = Repo.get!(Post, id)
+    post = Repo.get!(event_posts(conn.assigns[:event]), id)
     changeset = Post.changeset(post, post_params)
 
     case Repo.update(changeset) do
@@ -52,7 +67,7 @@ defmodule Churchspace.PostController do
   end
 
   def delete(conn, %{"id" => id}) do
-    post = Repo.get!(Post, id)
+    post = Repo.get!(event_posts(conn.assigns[:event]), id)
 
     # Here we use delete! (with a bang) because we expect
     # it to always work (and if it does not, it will raise).
