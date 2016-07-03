bf04d1b8da61de189142e52d1c2090f68bd83771
Timothy Wang <timzwang@gmail.com>
2016-07-02 17:39:08 -0700

    Add url helper for post/event-post if needed

--- web/path_helpers.ex
+++ web/path_helpers.ex
@@ -9,4 +9,13 @@ defmodule Churchspace.PathHelpers do
         post_path(conn, route, params)
     end
   end
+
+  def url_for_post(conn, route, params \\ []) when is_atom(route) do
+    case conn.assigns do
+      %{event: event = %Churchspace.Event{}} ->
+        event_post_url(conn, route, event, params)
+      _ ->
+        post_url(conn, route, params)
+    end
+  end
 end
