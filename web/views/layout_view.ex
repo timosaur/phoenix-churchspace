b5272354501607d0f14e8b10feef4e698e10c558
Timothy Wang <timzwang@gmail.com>
2016-07-19 23:46:31 -0700

    Set body[data-js-path] to allow JS page detection
    * js_path returns path from view_module and view_template
      e.g. `event/index.html` or `post/edit.html`

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,8 @@
 defmodule Churchspace.LayoutView do
   use Churchspace.Web, :view
+
+  def js_path(view_module, view_template) do
+    [Phoenix.Naming.resource_name(view_module, "View"), view_template]
+    |> Enum.join("/")
+  end
 end
