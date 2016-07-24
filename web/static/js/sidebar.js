512563207c33dba01c98a19f666b4c87136bf084
Timothy Wang <timzwang@gmail.com>
2016-07-23 23:51:24 -0700

    Add overlay and sidebar-toggle transitions

--- web/static/js/sidebar.js
+++ web/static/js/sidebar.js
@@ -4,5 +4,13 @@ export function loadSidebar() {
   $('[data-toggle=sidebar]').sidr({
     name: 'sidebar',
     displace: false,
+    onOpen: function() {
+      $('[data-toggle=sidebar]').addClass('active');
+      $('.overlay').addClass('active');
+    },
+    onClose: function() {
+      $('[data-toggle=sidebar]').removeClass('active');
+      $('.overlay').removeClass('active');
+    },
   });
 }
